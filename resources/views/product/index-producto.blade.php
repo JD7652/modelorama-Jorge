ad6078fa2e3626f33882ce54aeba90f2e4fec9e3
Jorge del Carmen <pyxrls@gmail.com>
2024-08-29 23:46:36 -0600

    Hicimos últimos cambios, agregamos el botón de regresar en las vistas show, create, y edit. También agregamos un botón para poder crear un producto en la vista index. Esos fueron los últimos cambios desde que le agregamos la card a show y el formulario para poder editar. :)

--- resources/views/product/index-producto.blade.php
+++ resources/views/product/index-producto.blade.php
@@ -23,6 +23,8 @@
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"><a href="https://flowbite.com/" class="hover:underline">Jorge del Carmen</a></span>
+        <a href="{{ route('producto.create') }}" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+            Crear Producto</a>
     </div>
 </footer>
 
--- resources/views/product/show-producto.blade.php
+++ resources/views/product/show-producto.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div class="flex justify-center items-center min-h-screen">
     <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nombre: </h5>
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-500 dark:text-white">{{$productDetail->name}}</h5>
@@ -17,6 +18,7 @@
             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
+            
         </a>
 
         <form action="{{route('producto.destroy', $productDetail)}}" method="POST">
@@ -25,6 +27,13 @@
             <br>
             <button type="submit" class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Eliminar</button>
         </form>
+        
+        <br>
+        
+        <a href="{{ route('producto.index') }}" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            << Regresar</a>
+        
+
     </div>
 </div>
 
